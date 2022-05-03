c2c506e9b2a291e3a5fa4cfbda1ae75c4bd05501
Dipok Roy <roydipok022@gmail.com>
2022-05-03 22:58:05 +0600

    home page slider added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Carousel } from "react-bootstrap";
+import banner1 from "../../imade/banner-1.jpg";
+import banner2 from "../../imade/banner-2.jpg";
+import banner3 from "../../imade/banner-3.jpg";
 
 const Home = () => {
   return (
     <div>
-      <h1>This is home hear</h1>
+      <Carousel>
+        <Carousel.Item>
+          <img className="d-block w-100" src={banner1} alt="First slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={banner2} alt="Second slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={banner3} alt="Third slide" />
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 };
