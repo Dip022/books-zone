2b55c98141bc838789b60a686fb5de2b2b8d4726
Dipok Roy <roydipok022@gmail.com>
2022-05-04 01:22:38 +0600

    Sign In added

--- src/Pages/Sign In/SignIn.js
+++ src/Pages/Sign In/SignIn.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import "./SignIn.css";
 
 const SignIn = () => {
   return (
-    <div>
-      <h1>This is a Sign In</h1>
+    <div className="signIn-container ">
+      <Container>
+        <div className="sign-in mx-auto">
+          <h3 className="text-center">Sign In</h3>
+          <form>
+            <label>Email Address</label>
+            <input
+              type="email"
+              name="email"
+              id=""
+              placeholder="Eamil Address"
+              required
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              id=""
+              required
+              placeholder="Password"
+            />
+            <input className="submit" type="submit" value="Sign In" />
+          </form>
+          <p>Need an Account?</p>
+          <p>Forgot Password?</p>
+        </div>
+      </Container>
     </div>
   );
 };
