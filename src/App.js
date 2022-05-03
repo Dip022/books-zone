8c90288e62c3360c32b50ecbfe6e28313faaea6a
Dipok Roy <roydipok022@gmail.com>
2022-05-03 22:15:33 +0600

    pages added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Blog from "./Pages/Blog/Blog";
+import Header from "./Pages/Header/Header";
+import Home from "./Pages/Home/Home";
+import Inventories from "./Pages/Inventories/Inventories";
+import SignIn from "./Pages/Sign In/SignIn";
+import SignUp from "./Pages/Sign Up/SignUp";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route
+          path="/inventories"
+          element={<Inventories></Inventories>}
+        ></Route>
+        <Route path="/blog" element={<Blog></Blog>}></Route>
+        <Route path="/signin" element={<SignIn></SignIn>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Inventories/Inventories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Inventories = () => {
+  return (
+    <div>
+      <h1>This is Inventories page heir</h1>
+    </div>
+  );
+};
+
+export default Inventories;
