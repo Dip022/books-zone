4b367eba9d3ad76269413068f8fe306a9ff2768f
Dipok Roy <roydipok022@gmail.com>
2022-05-04 10:20:04 +0600

    sing up page added

--- src/Pages/Sign Up/SignUp.js
+++ src/Pages/Sign Up/SignUp.js
@@ -1,9 +1,68 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import google from "../../imade/google.png";
 
 const SignUp = () => {
+  const handelSignUp = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const conPassword = event.target.conPassword.value;
+    console.log(name, email, password, conPassword);
+  };
   return (
-    <div>
-      <h1>This is a Sign Up</h1>
+    <div className="signIn-container ">
+      <Container>
+        <div className="form-area mx-auto">
+          <div className="sign-in">
+            <h3 className="text-center">Sign Up</h3>
+            <form onSubmit={handelSignUp}>
+              <label>Name</label>
+              <input
+                type="text"
+                name="name"
+                id=""
+                placeholder="Name"
+                required
+              />
+              <label>Email Address</label>
+              <input
+                type="email"
+                name="email"
+                id=""
+                placeholder="Eamil Address"
+                required
+              />
+              <label>Password</label>
+              <input
+                type="password"
+                name="password"
+                id=""
+                required
+                placeholder="Password"
+              />
+              <label>Confirm Password</label>
+              <input
+                type="password"
+                name="conPassword"
+                id=""
+                required
+                placeholder="Confirm Password"
+              />
+              <input className="submit" type="submit" value="Sign In" />
+            </form>
+            <p>Have an Account? </p>
+          </div>
+          <div className="social-login">
+            <span className="text-center d-block">or</span>
+            <button>
+              <img src={google} alt="" />
+              Google Sign In
+            </button>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
