3096de72af507300afa467c29e93487f93e7d292
Dipok Roy <roydipok022@gmail.com>
2022-05-05 14:48:47 +0600

    add item desing added

--- src/Pages/Add Item/AddItem.js
+++ src/Pages/Add Item/AddItem.js
@@ -1,9 +1,38 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import "./AddItem.css";
 
 const AddItem = () => {
   return (
-    <div>
-      <h1>Add Item</h1>
+    <div className="add-item-container mb-5">
+      <h1 className="text-center mt-5 bm-5">Add Item</h1>
+      <Container>
+        <div className="add-item">
+          <div className="update-fild">
+            <form>
+              <input type="text" name="name" placeholder="Product Name" />
+              <input type="number" name="price" placeholder="Product Price" />
+              <input type="number" name="stockBook" placeholder="Stock Book" />
+              <input
+                type="text"
+                name="supplierName"
+                placeholder="Supplier Name"
+              />
+              <input
+                type="text"
+                name="product-Image"
+                placeholder="Product Image URL"
+              />
+              <textarea
+                type="text"
+                name="shortDescription"
+                placeholder="Short Description"
+              ></textarea>
+              <input className="item-submit" type="submit" value="Add Item" />
+            </form>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
